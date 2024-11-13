b18b0b22a6046ea93de9bfa4c1405483d172245b
Ruan <oliveiraruan2018@gmail.com>
2024-11-13 18:57:24 -0400

    domain model

---
+++ dvgame-list/src/main/java/com/devruan/listgames/entities/Belonging.java
@@ -0,0 +1,48 @@
+package com.devruan.listgames.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belongin")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+    public Game getGame(){
+        return id.getGame();
+    }
+    public void setGame(Game game){
+        id.setGame(game);
+    }
+    public GameList getGameList(){
+        return id.getList();
+    }
+    public void setGameList(GameList gameList){
+        id.setList(gameList);
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
